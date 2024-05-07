2410a05bbc33d62cf03cbb02682329e5d5555d8d
Nick444-del <nickgoruleax000e1000@gmail.com>
2024-05-07 20:25:45 +0530

    Completed 7th assigment.

--- JavaScript/Assigment/Assigment 7/script.js
+++ JavaScript/Assigment/Assigment 7/script.js
@@ -1,9 +1,11 @@
 var modal1 = document.getElementById('modal1');
 
-function open_modal(i, m_t){
+function open_modal(a, m_t, c){
     modal1.style.display = "block";
     var title = document.getElementById('m-title');
     title.innerHTML = m_t;
+    var image = document.getElementById('m-change');
+    image.src = './img/pic'+c+'.png';
 }
 
 function close2(){
