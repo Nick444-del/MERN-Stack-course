50526707b39ae66d65bfc3aa37efab0ae28d18f2
Nick444-del <nickgoruleax000e1000@gmail.com>
2024-06-07 11:23:59 +0530

    Completed session and Assigment

---
+++ Avdance JavaScript/Assigment/Assigment 5/script.js
@@ -0,0 +1,66 @@
+let dataLoader = () => {
+    let x = new XMLHttpRequest();
+
+    x.onload = function () {
+        if (this.status == 200 && this.readyState == 4) {
+            let a = JSON.parse(this.responseText);
+            let maindata = document.querySelector('#main');
+            a.forEach((e) => {
+                let box = document.createElement('div');
+                box.className = 'box';
+                maindata.appendChild(box); 
+
+                for (let i in e) {
+                    if (i == 'id') {
+                        let h1 = document.createElement('h1');
+                        box.appendChild(h1);
+                        h1.innerHTML = `${i} ${e[i]}`;
+                    } else if (i == 'title') {
+                        let h2 = document.createElement('h2');
+                        box.appendChild(h2);
+                        h2.innerHTML = `<b>${i}</b>: ${e[i]}`;
+                    } else if (i == 'body') {
+                        let p = document.createElement('p');
+                        box.appendChild(p);
+                        p.innerHTML = `<b>${i}</b>: ${e[i]}`;
+                    }
+                }
+                let btn1 = document.createElement('button');
+                box.appendChild(btn1);
+                btn1.innerHTML = 'Read More';
+                btn1.addEventListener('click', () => {
+                    let modal = document.createElement('div');
+                    modal.className = 'modal';
+                    modal.style.overflow = 'auto';
+                    document.body.appendChild(modal);
+                    modal.style.display = 'flex';
+                    modal.style.alignItems = 'center';
+                    modal.style.justifyContent = 'center';
+                    let card = document.createElement('div');
+                    card.className = 'card';
+                    modal.appendChild(card);
+                    let card_h1 = document.createElement('h1');
+                    card.appendChild(card_h1);
+                    card_h1.innerHTML = "ID "+e.id;
+                    let card_h2 = document.createElement('h2');
+                    card.appendChild(card_h2);
+                    card_h2.innerHTML = "Title: "+e.title;
+                    let card_body = document.createElement('p');
+                    card.appendChild(card_body);
+                    card_body.innerHTML = "Body: "+e.body;
+                    let btn_close = document.createElement('button');
+                    card.appendChild(btn_close);
+                    btn_close.innerHTML = 'Close';
+                    btn_close.addEventListener('click', () => {
+                        modal.style.display = 'none';
+                    });
+                });
+            });
+            
+        }
+    }
+    x.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
+    x.send();
+};
+
+dataLoader();
---
+++ Avdance JavaScript/Session's/Session 6/script.js
@@ -0,0 +1,89 @@
+// exception/error handling
+// let a = 'ABC';
+// try{
+//     console.log(a);
+// }catch{
+//     console.log('error');
+// }
+// console.log('Hello');
+
+// function fun1(){
+    // let name = document.querySelector('#name').value;
+    // let dName = document.querySelector('#dName');
+    // document.querySelector('#name').style.backgroundColor = "white";
+    // document.querySelector('#name').value = '';
+
+    // try{
+    //     if(name == "") throw "empty";
+    //     if(isNaN(name)) throw "not a number";
+        
+    //     name = Number(name);
+    //     if(name < 5) throw "too low";
+    //     if(name >= 10) throw "too high";
+    //     else{
+    //         document.querySelector("#name").style.backgroundColor = "aqua";
+            
+    //     }
+    // }catch(err){
+    //     dName.innerHTML = "Output is "+ err;
+    // }
+
+    // let name = document.querySelector('#name').value;
+    // let dName = document.querySelector('#dName');
+    // document.querySelector('#name').style.backgroundColor = "white";
+    // document.querySelector('#name').value = "";
+    // let reg = /[0-9]/g;
+    // let reg2 = /[!@#\$%\^\&*\)\(+=._-]/g
+
+    // try{
+    //     if(!isNaN(name)) throw ' a number';
+    //     if(name == "") throw ' empty';
+    //     if(name.match(reg)) throw ' number and string not allowed';
+    //     if(name.match(reg2)) throw ' special character not allowed';
+    //     else{
+    //         document.querySelector('#name').style.backgroundColor = "aqua";
+    //         dName.innerHTML = name.toUpperCase();
+    //     }
+    // }catch(err){
+    //     dName.innerHTML = "Output is "+ err;
+    // }
+// }
+
+let fun2 = () => {
+    let x = new XMLHttpRequest(); // XHR
+    
+    x.onload = function(){
+        if(this.status == 200 && this.readyState == 4){
+            // document.write(JSON.parse(this.responseText));
+            let a = JSON.parse(this.responseText);
+            let maindata = document.querySelector('#box');
+            a.forEach((e) => {
+                let div1 = document.createElement('div');
+                div1.className = 'abc';
+                maindata.appendChild(div1);
+
+                for(let i in e){
+                    if(i == 'id'){
+                        let h1 = document.createElement('h1');
+                        div1.appendChild(h1);
+                        h1.innerHTML =  `<b>${i}</b> ${e[i]}`;
+                    }else if(i == 'title' || i == 'price' || i == 'description'){
+                        let p = document.createElement('p');
+                        div1.appendChild(p);
+                        p.innerHTML = `<b>${i}</b> ${e[i]}`;
+                    }else if(i == 'image'){
+                        let img = document.createElement('img');
+                        div1.appendChild(img);
+                        img.src = e[i];
+                    }
+                }
+            });
+        }else{
+            document.write("URL not working");
+        }
+    }
+    x.open("GET", 'https://fakestoreapi.com/products', true);
+    x.send();
+};
+
+fun2();
