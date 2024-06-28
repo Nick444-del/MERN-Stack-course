692109cd2680383f91403a1c0ab75ff1f26fed62
Nick444-del <nickgoruleax000e1000@gmail.com>
2024-06-28 20:45:06 +0530

    updates

---
+++ PHP/Session 1/index.php
@@ -0,0 +1,32 @@
+<?php
+    // echo 'test';
+
+    # echo 'test';
+
+    /* echo 'test'; */
+
+    // echo '<h1>Hello World!</h1>';
+    // echo 'Testing PHP <br>';
+    // $a = 'Hello';
+    // $b = 231;
+    // $c = false;
+    // echo $a . ' ' . $b . ' ' . $c;
+
+    // var_dump($a, $b, $c);
+
+    $myname = 'ABC';
+    $my_name = 'ABC';
+    $myName = 'ABC';
+    $_myname = 'ABC';
+    $_my_name = 'ABC';
+    $_myName = 'ABC';
+    $myname1 = 'ABC';
+
+    echo $myname. '<br>';
+    echo $my_name. '<br>';
+    echo $myName. '<br>';
+    echo $_myname. '<br>';
+    echo $_my_name. '<br>';
+    echo $_myName. '<br>';
+    echo $myname1. '<br>';
+?>
