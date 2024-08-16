9892b6592eb434227e5b6429201757103fdb8fc9
Nick444-del <nickgoruleax000e1000@gmail.com>
2024-08-16 14:59:12 +0530

    Update

--- Node js/Practise/Practise 3/function.js
+++ Node js/Practise/Practise 3/function.js
@@ -22,3 +22,71 @@ export const contactFn = (req, res) => {
     ];
     res.send(JSON.stringify(userData));
 }
+
+export const dataFn = (req, res) => {
+    try {
+        const info = [
+            {
+                name: "John",
+                email: "j@j.com",
+                phone: "1234567890",
+                age: 23,
+                gender: "Male",
+                city: "Delhi",
+                country: "India"
+            },
+            {
+                name: "Jane",
+                email: "j@j.com",
+                phone: "1234567890",
+                age: 23,
+                gender: "Female",
+                city: "Delhi",
+                country: "India"
+            },
+            {
+                name: "Jim",
+                email: "j@j.com",
+                phone: "1234567890",
+                age: 23,
+                gender: "Male",
+                city: "Delhi",
+                country: "India"
+            },
+            {
+                name: "Jill",
+                email: "j@j.com",
+                phone: "1234567890",
+                age: 23,
+                gender: "Female",
+                city: "Delhi",
+                country: "India"
+            }
+        ];
+        res.status(200).json({
+            data: info,
+            message: "Fetch user Data",
+            success: true
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+            success: false
+        })
+    }
+}
+
+export const abc = (req, res) => {
+    try{
+        console.log(req.body);
+        res.send(201).json({
+            message: "Data Inserted",
+            success: true
+        })
+    }catch(error){
+        res.status(500).json({
+            message: error.message,
+            success: false
+        })
+    }
+}
--- Node js/Practise/Practise 3/index.js
+++ Node js/Practise/Practise 3/index.js
@@ -1,15 +1,19 @@
 import express from "express";
-import { aboutFn, contactFn } from "./function.js";
+import { aboutFn, contactFn, dataFn, abc } from "./function.js";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Home");
 });
 
 app.get("/about", aboutFn);
 app.get("/contact", contactFn);
+app.get("/data", dataFn);
+app.post("/abc", abc);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
