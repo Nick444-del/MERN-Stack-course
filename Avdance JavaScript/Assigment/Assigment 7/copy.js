9af38a179fa67bb1036b23632e9b06894ec331b1
Nick444-del <nickgoruleax000e1000@gmail.com>
2024-08-16 15:25:55 +0530

    New functions

--- Avdance JavaScript/Assigment/Assigment 7/copy.js
+++ Avdance JavaScript/Assigment/Assigment 7/copy.js
@@ -11,7 +11,6 @@ function fun1(){
             sessionStorage.setItem("data",a)
         }
     }
-
 }
 
 fun1()
--- Avdance JavaScript/Assigment/Assigment 7/script.js
+++ Avdance JavaScript/Assigment/Assigment 7/script.js
@@ -67,7 +67,7 @@ function categorySearch() {
                         let card = document.createElement('div');
                         card.className = 'card';
                         right.appendChild(card);
-                        for(let i in e){
+                        for (let i in e) {
                             if (i == 'title') {
                                 let heading = document.createElement('h4');
                                 card.appendChild(heading);
@@ -109,5 +109,121 @@ function categorySearch() {
         x.open('GET', 'https://fakestoreapi.com/products', true);
         x.send();
     };
+    dataLoader();
+}
+
+function upToHundred() {
+    let dataLoader = () => {
+        let x = new XMLHttpRequest();
+        x.onload = function () {
+            if (this.status == 200 && this.readyState == 4) {
+                let a = JSON.parse(this.responseText);
+                let right = document.querySelector('#right');
+                right.innerHTML = '';
+                a.forEach((e) => {
+                    if (e.price <= 100) {
+                        let card = document.createElement('div');
+                        card.className = 'card';
+                        right.appendChild(card);
+                        for (let i in e) {
+                            if (i == 'title') {
+                                let heading = document.createElement('h4');
+                                card.appendChild(heading);
+                                heading.innerHTML = e[i];
+                            }
+                            if (i == 'category') {
+                                let tag = document.createElement('p');
+                                card.appendChild(tag);
+                                tag.className = 'tag';
+                                tag.innerHTML = e[i];
+                            }
+                            if (i == 'image') {
+                                let img = document.createElement('img');
+                                card.appendChild(img);
+                                img.className = 'img';
+                                img.src = e[i];
+                            }
+                            if (i == 'price') {
+                                let price = document.createElement('p');
+                                card.appendChild(price);
+                                price.className = 'price';
+                                price.innerHTML = `$${e[i]}`;
+                            }
+                            if (i == 'description') {
+                                let desc = document.createElement('p');
+                                card.appendChild(desc);
+                                desc.className = 'desc';
+                                desc.innerHTML = e[i];
+                            }
+                        }
+                        let btn = document.createElement('button');
+                        card.appendChild(btn);
+                        btn.innerHTML = 'Add To Cart';
+                        btn.className = 'btn';
+                    }
+                })
+            }
+        }
+        x.open('GET', 'https://fakestoreapi.com/products', true);
+        x.send();
+    };
+    dataLoader();
+}
+
+function rating() {
+    let dataLoader = () => {
+        let x = new XMLHttpRequest();
+        x.onload = function () {
+            if (this.status == 200 && this.readyState == 4) {
+                let a = JSON.parse(this.responseText);
+                let right = document.querySelector('#right');
+                right.innerHTML = '';
+                a.forEach((e) => {
+                    if (e.rate >= document.querySelector('#low') && e.rate <= document.querySelector('#high')) {
+                        let card = document.createElement('div');
+                        card.className = 'card';
+                        right.appendChild(card);
+                        for (let i in e) {
+                            if (i == 'title') {
+                                let heading = document.createElement('h4');
+                                card.appendChild(heading);
+                                heading.innerHTML = e[i];
+                            }
+                            if (i == 'category') {
+                                let tag = document.createElement('p');
+                                card.appendChild(tag);
+                                tag.className = 'tag';
+                                tag.innerHTML = e[i];
+                            }
+                            if (i == 'image') {
+                                let img = document.createElement('img');
+                                card.appendChild(img);
+                                img.className = 'img';
+                                img.src = e[i];
+                            }
+                            if (i == 'price') {
+                                let price = document.createElement('p');
+                                card.appendChild(price);
+                                price.className = 'price';
+                                price.innerHTML = `$${e[i]}`;
+                            }
+                            if (i == 'description') {
+                                let desc = document.createElement('p');
+                                card.appendChild(desc);
+                                desc.className = 'desc';
+                                desc.innerHTML = e[i];
+                            }
+                        }
+                        let btn = document.createElement('button');
+                        card.appendChild(btn);
+                        btn.innerHTML = 'Add To Cart';
+                        btn.className = 'btn';
+                    }
+                })
+            }
+        }
+        x.open('GET', 'https://fakestoreapi.com/products', true);
+        x.send();
+    }
     dataLoader();
 }
